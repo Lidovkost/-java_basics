41dd595a03deacf94ed33cdd53187a6436c3c588
«Lidovskiy_Konstantin» <lidovkost@gmail.com>
2023-03-11 03:19:02 +0300

    logs fo calculator

--- HomeWork1/Ex3.java
+++ HomeWork1/Ex3.java
@@ -1,15 +1,21 @@
 package homework.HomeWork1;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 public class Ex3 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.print("Input a number 1: ");
         Double num1 = in.nextDouble();
-        System.out.print("Input operation: ");
+        System.out.print("Input operation(or end to exit): ");
         String op = in.next();
         System.out.print("Input a number 2: ");
         Double num2 = in.nextDouble();
-        System.out.printf("%g %s %g = %g \n", num1, op, num2, DoOperation(op.charAt(0), num1, num2));
+        String text = String.format("%g %s %g = %g \n", num1, op, num2, DoOperation(op.charAt(0), num1, num2));
+        System.out.printf(text);
+        WriteTofile("file.log", text);               
         in.close();
     }
     static Double DoOperation(char op, double n1, double n2){
@@ -27,5 +33,36 @@ public class Ex3 {
         }
         else
             return null;
+    }
+    static void WriteTofile(String path, String text){
+        String oldText = ReadFile(path);
+        try(FileWriter writer = new FileWriter(path, false))
+        {
+            writer.write(oldText);
+            writer.append(text);
+            writer.flush();
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    static String ReadFile(String path){
+        String str = "";
+        String result = "";
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+        while ((str = br.readLine()) != null) {
+        result = result + str + '\n';
+        }
+
+        br.close();
+        return result;
+        }
+        catch(IOException ex){
+             
+            System.out.println(ex.getMessage());
+        }
+        return result;
     }
 }
